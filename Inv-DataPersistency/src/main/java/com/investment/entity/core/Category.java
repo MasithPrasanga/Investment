db7beabc66c3adfb16bba957258b5899fa87e6a2
masith <masithprasanga@gmail.com>
2016-12-05 10:18:02 +0530

    Added Currency,Category,CustomerType,Type Core Entity Classes

---
+++ Inv-DataPersistency/src/main/java/com/investment/entity/core/Category.java
@@ -0,0 +1,53 @@
+package com.investment.entity.core;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true, nullable = false)
+	private int id;
+	
+	@Column(name = "name", unique = true, nullable = false)
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Inv-DataPersistency/src/main/java/com/investment/entity/core/CustomerType.java
@@ -0,0 +1,47 @@
+package com.investment.entity.core;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "customer_type")
+public class CustomerType implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true, nullable = false)
+	private int id;
+	
+	@Column(name = "type", unique = true, nullable = false)
+	private String type;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
+
+
+
+
