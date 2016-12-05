3c9a71d2805d34018ce4db7f2c4311a067135d03
masith <masithprasanga@gmail.com>
2016-12-05 11:31:45 +0530

    Added Dto classes for Core Entity Classes

---
+++ Inv-RestApi/src/main/java/com/investment/dto/core/Type.java
@@ -0,0 +1,22 @@
+package com.investment.dto.core;
+
+import java.io.Serializable;
+
+public class Type implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
+
+
+
