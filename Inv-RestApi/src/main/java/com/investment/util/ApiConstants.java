72641cee221f14b751100a018aa7d0dcac87e8e9
masith <masithprasanga@gmail.com>
2016-12-06 11:24:50 +0530

    Developed ApiConstants Interface

---
+++ Inv-RestApi/src/main/java/com/investment/util/ApiConstants.java
@@ -0,0 +1,16 @@
+package com.investment.util;
+
+public interface ApiConstants {
+
+	// constants related to CoreUser  
+	public static final String AdminApproved = "Approved";
+	public static final String AdminNotApproved = "NotApproved";
+	
+	// constants related to UserRole  
+	public static final String Admin = "Admin";
+	public static final String Entrepreneur = "Entrepreneur";
+	public static final String Investor = "Investor";
+	
+}
+
+
