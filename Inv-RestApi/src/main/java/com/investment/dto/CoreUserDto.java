8e54ccb930141be7426ded4cb829fdbd631adbf4
masith <masithprasanga@gmail.com>
2016-12-06 11:27:11 +0530

    Developed CoreUserDto Class

---
+++ Inv-RestApi/src/main/java/com/investment/dto/CoreUserDto.java
@@ -0,0 +1,189 @@
+package com.investment.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class CoreUserDto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String firstName;
+
+	private String lastName;
+
+	private String userEmail;
+
+	private String password;
+
+	private String mobileNumber;
+
+	private String landNumber;
+
+	private Date birthDate;
+	
+	private String gender;
+
+	private String accountType;
+
+	private String activationCode;
+	
+	private Date createdDate;
+
+	private Date activatedDate;
+
+	private String activationStatus;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getLandNumber() {
+		return landNumber;
+	}
+
+	public void setLandNumber(String landNumber) {
+		this.landNumber = landNumber;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public String getActivationCode() {
+		return activationCode;
+	}
+
+	public void setActivationCode(String activationCode) {
+		this.activationCode = activationCode;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getActivatedDate() {
+		return activatedDate;
+	}
+
+	public void setActivatedDate(Date activatedDate) {
+		this.activatedDate = activatedDate;
+	}
+
+	public String getActivationStatus() {
+		return activationStatus;
+	}
+
+	public void setActivationStatus(String activationStatus) {
+		this.activationStatus = activationStatus;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
