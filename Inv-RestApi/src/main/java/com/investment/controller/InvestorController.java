372b5631545f57792e69f61ed7146967230bcd48
masith <masithprasanga@gmail.com>
2016-12-07 14:18:53 +0530

    Developed End Point to show all the admin approved proposals to the investor

--- Inv-RestApi/src/main/java/com/investment/controller/InvestorController.java
+++ Inv-RestApi/src/main/java/com/investment/controller/InvestorController.java
@@ -1,9 +1,32 @@
 package com.investment.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.investment.entity.ProcessedProjectInfo;
+import com.investment.service.ProcessedProjectInfoService;
+
 @RestController
+@RequestMapping("api/v1/investor")
 public class InvestorController {
 
+	@Autowired
+	private ProcessedProjectInfoService processedProjectInfoService = null;
+	
+	// Show All the Approved project List after sign in 
+	@RequestMapping(value = "/getnewprojects", method = RequestMethod.GET)
+	public ResponseEntity<List<ProcessedProjectInfo>> getAllNewProjects() {
+		List<ProcessedProjectInfo> approvedProposalList = processedProjectInfoService.getAllRecords();
+		if (approvedProposalList.isEmpty()) {
+			return new ResponseEntity<List<ProcessedProjectInfo>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<ProcessedProjectInfo>>(approvedProposalList, HttpStatus.OK);
+	}
 }
 
