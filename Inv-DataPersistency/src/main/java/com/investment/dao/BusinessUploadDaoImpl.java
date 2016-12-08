0ddd0d7a8a2c58f2f32a757b4a1e574c4f9a324b
masith <masithprasanga@gmail.com>
2016-12-09 01:37:48 +0530

    Implemented the findListById method in DaoImpl class with Transaction handling

--- Inv-DataPersistency/src/main/java/com/investment/dao/BusinessUploadDaoImpl.java
+++ Inv-DataPersistency/src/main/java/com/investment/dao/BusinessUploadDaoImpl.java
@@ -1,12 +1,44 @@
 package com.investment.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.investment.dao.root.RootDaoImpl;
 import com.investment.entity.BusinessUpload;
+import com.investment.entity.RawProjectInfo;
 
 @Repository
 public class BusinessUploadDaoImpl extends RootDaoImpl<BusinessUpload> implements BusinessUploadDao{
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public List<BusinessUpload> findListById(RawProjectInfo rawProjectInfo) {
+		
+		Transaction transaction = null;
+		Session session =  null;
+		
+		try{
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			int id = rawProjectInfo.getId();
+			String hql = "FROM BusinessUpload B WHERE B.rawData = "+id;
+			Query query = session.createQuery(hql);
+			List<BusinessUpload> businessUploadList = query.list();
+			transaction.commit();
+			return businessUploadList;
+		}catch(Exception e){
+			e.printStackTrace();
+			transaction.rollback();
+			return null;
+		}
+	}
 
 }
 
