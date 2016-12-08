98a81f6146b411c7951801634247a5d929a3c477
masith <masithprasanga@gmail.com>
2016-12-09 01:40:03 +0530

    Developed api/v/admin/singleproposaldetails End Point

--- Inv-RestApi/src/main/java/com/investment/controller/AdminController.java
+++ Inv-RestApi/src/main/java/com/investment/controller/AdminController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.investment.dto.ProcessedProjectInfoDto;
 import com.investment.dto.response.RawProjectInfoResponseDto;
+import com.investment.dto.response.RawProposalResponseDto;
 import com.investment.entity.BusinessUpload;
 import com.investment.entity.RawProjectInfo;
 import com.investment.handler.AdminHandler;
 import com.investment.service.BusinessUploadService;
-import com.investment.service.CoreUserService;
 import com.investment.service.RawProjectInfoService;
 import com.investment.util.ApiConstants;
 
@@ -26,44 +27,41 @@ import com.investment.util.ApiConstants;
 public class AdminController {
 
 	@Autowired
-	private CoreUserService coreUserService = null;
-	
+	private BusinessUploadService businessUploadService = null;
+
 	@Autowired
-	private BusinessUploadService businessUploadService= null;
+	private RawProjectInfoService rawProjectInfoService = null;
 	
 	@Autowired
 	private AdminHandler adminHandler = null;
-	
-	@Autowired
-	private RawProjectInfoService rawProjectInfoService = null;
 
 	// processing the newly added proposals and approving it
 	@RequestMapping(value = "/approveproposal", method = RequestMethod.POST)
 	public ResponseEntity<Void> saveProcessedData(@RequestBody ProcessedProjectInfoDto processedProjectInfoDto) {
-		try{
+		try {
 			boolean status = adminHandler.createProject(processedProjectInfoDto);
-			if(status){
+			if (status) {
 				return new ResponseEntity<Void>(HttpStatus.CREATED);
 			}
-			return new ResponseEntity<Void>(HttpStatus.EXPECTATION_FAILED);	
-		}catch(Exception e){
+			return new ResponseEntity<Void>(HttpStatus.EXPECTATION_FAILED);
+		} catch (Exception e) {
 			return new ResponseEntity<Void>(HttpStatus.EXPECTATION_FAILED);
 		}
 	}
 
-	// showing the newly submitted proposals to the admin (This end point is very slow)
+	// showing the newly submitted proposals to the admin
 	@RequestMapping(value = "/newproposals", method = RequestMethod.GET)
 	public ResponseEntity<List<RawProjectInfoResponseDto>> getAllNewlySubmitedProposals() {
-		
+
 		List<RawProjectInfo> rawProjectInfoList = rawProjectInfoService.getAllRecords();
-		
+
 		if (rawProjectInfoList.isEmpty()) {
 			return new ResponseEntity<List<RawProjectInfoResponseDto>>(HttpStatus.NO_CONTENT);
 		}
-		
+
 		List<RawProjectInfoResponseDto> rawProjectInfoResponseList = new ArrayList<RawProjectInfoResponseDto>();
-		for(RawProjectInfo r: rawProjectInfoList){
-			if(r.getAdminStatus().equals(ApiConstants.ADMIN_NOT_APPROVED)){
+		for (RawProjectInfo r : rawProjectInfoList) {
+			if (r.getAdminStatus().equals(ApiConstants.ADMIN_NOT_APPROVED)) {
 				RawProjectInfoResponseDto rawProjectInfoResponse = new RawProjectInfoResponseDto();
 				rawProjectInfoResponse.setId(r.getId());
 				rawProjectInfoResponse.setProjectName(r.getProjectName());
@@ -72,13 +70,43 @@ public class AdminController {
 				rawProjectInfoResponseList.add(rawProjectInfoResponse);
 			}
 		}
-		
+
 		return new ResponseEntity<List<RawProjectInfoResponseDto>>(rawProjectInfoResponseList, HttpStatus.OK);
 	}
-	
-	// Get the Specific newly added proposal details
-	
+
+	// Get the Specific newly added proposal details (try to develop with using newly written findListById service method)
+	@RequestMapping(value = "/singleproposaldetails/{id}", method = RequestMethod.POST)
+	public ResponseEntity<List<RawProposalResponseDto>> getSingleProposalDetails(@PathVariable("id") int rawProjectInfoId) {
+		
+		try{
+			RawProjectInfo rawProjectInfo = rawProjectInfoService.findById(rawProjectInfoId);
+			List<BusinessUpload> allBusinessUploadList = businessUploadService.findListById(rawProjectInfo);
+			List<RawProposalResponseDto> rawProposalResponseList = new ArrayList<RawProposalResponseDto>();
+			for(BusinessUpload b :allBusinessUploadList){
+				RawProposalResponseDto rawProposalResponseDto = new RawProposalResponseDto();
+				rawProposalResponseDto.setId(b.getId());
+				rawProposalResponseDto.setDate(b.getDate());
+				rawProposalResponseDto.setUrl(b.getUrl());
+				rawProposalResponseList.add(rawProposalResponseDto);
+			}
+			return new ResponseEntity<List<RawProposalResponseDto>>(rawProposalResponseList,HttpStatus.OK);
+			
+		}catch(Exception e){
+			return new ResponseEntity<List<RawProposalResponseDto>>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
