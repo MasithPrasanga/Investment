01a73ecfa867fe24c9dea746e57382b68683bf95
masith <masithprasanga@gmail.com>
2016-12-09 01:36:35 +0530

    Added findListById Abstract method to BusinessUploadDao Interface

--- Inv-DataPersistency/src/main/java/com/investment/dao/BusinessUploadDao.java
+++ Inv-DataPersistency/src/main/java/com/investment/dao/BusinessUploadDao.java
@@ -1,10 +1,14 @@
 package com.investment.dao;
 
+import java.util.List;
+
 import com.investment.dao.root.RootDao;
 import com.investment.entity.BusinessUpload;
+import com.investment.entity.RawProjectInfo;
 
 public interface BusinessUploadDao  extends RootDao<BusinessUpload>{
 
+	public List<BusinessUpload> findListById(RawProjectInfo rawProjectInfo);
 }
 
 
