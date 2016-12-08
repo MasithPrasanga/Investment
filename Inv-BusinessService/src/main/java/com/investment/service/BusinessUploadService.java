063e88fe3e5b71538786e90f921da18b4d1f67d0
masith <masithprasanga@gmail.com>
2016-12-09 01:33:47 +0530

    Added  findListById Abstract method to BusinessUploadService

--- Inv-BusinessService/src/main/java/com/investment/service/BusinessUploadService.java
+++ Inv-BusinessService/src/main/java/com/investment/service/BusinessUploadService.java
@@ -1,8 +1,13 @@
 package com.investment.service;
 
+import java.util.List;
+
 import com.investment.entity.BusinessUpload;
+import com.investment.entity.RawProjectInfo;
 import com.investment.root.service.RootService;
 
 public interface BusinessUploadService extends RootService<BusinessUpload>{
-
+	
+	public List<BusinessUpload> findListById(RawProjectInfo rawProjectInfo);
+	
 }
