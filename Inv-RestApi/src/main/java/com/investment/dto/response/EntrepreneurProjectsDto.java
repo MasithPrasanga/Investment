587e0dc0c60266b2d307b3f11ab12748e3b18820
masith <masithprasanga@gmail.com>
2016-12-10 14:36:32 +0530

    Added EntrepreneurProjectsDto class to send the all the entrepreneur project list

---
+++ Inv-RestApi/src/main/java/com/investment/dto/response/EntrepreneurProjectsDto.java
@@ -0,0 +1,81 @@
+package com.investment.dto.response;
+
+import java.util.List;
+
+import com.investment.entity.CoreUser;
+
+public class EntrepreneurProjectsDto {
+
+	private CoreUser coreUser = null;
+	
+	private List<RawProjectInfoResponseDto> rawProjectList = null;
+	
+	private List<ProssedProjectInfoResponseDto> processedProjectList = null;
+
+	public CoreUser getCoreUser() {
+		return coreUser;
+	}
+
+	public void setCoreUser(CoreUser coreUser) {
+		this.coreUser = coreUser;
+	}
+
+	public List<RawProjectInfoResponseDto> getRawProjectList() {
+		return rawProjectList;
+	}
+
+	public void setRawProjectList(List<RawProjectInfoResponseDto> rawProjectList) {
+		this.rawProjectList = rawProjectList;
+	}
+
+	public List<ProssedProjectInfoResponseDto> getProcessedProjectList() {
+		return processedProjectList;
+	}
+
+	public void setProcessedProjectList(List<ProssedProjectInfoResponseDto> processedProjectList) {
+		this.processedProjectList = processedProjectList;
+	}
+
+	@Override
+	public String toString() {
+		return "EntrepreneurProjectsDto [coreUser=" + coreUser + ", rawProjectList=" + rawProjectList
+				+ ", processedProjectList=" + processedProjectList + "]";
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
