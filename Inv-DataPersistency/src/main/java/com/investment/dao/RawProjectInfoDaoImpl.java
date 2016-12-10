548846a42b6c62f173d3cb27f79b91174de3dade
masith <masithprasanga@gmail.com>
2016-12-10 14:45:12 +0530

    Developed  RawProjectInfoDaoImpl

--- Inv-DataPersistency/src/main/java/com/investment/dao/RawProjectInfoDaoImpl.java
+++ Inv-DataPersistency/src/main/java/com/investment/dao/RawProjectInfoDaoImpl.java
@@ -2,7 +2,11 @@ package com.investment.dao;
 
 import java.util.List;
 
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.investment.dao.root.RootDaoImpl;
@@ -10,19 +14,43 @@ import com.investment.entity.RawProjectInfo;
 
 @Repository
 @SuppressWarnings("unchecked")
-public class RawProjectInfoDaoImpl extends RootDaoImpl<RawProjectInfo> implements RawProjectInfoDao{
+public class RawProjectInfoDaoImpl extends RootDaoImpl<RawProjectInfo> implements RawProjectInfoDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	public List<RawProjectInfo> findByUserId(Integer userid) {
-		try{
-			return getSession().createCriteria(RawProjectInfo.class).add(Restrictions.eq("coreUser", userid)).list();
-		}
-		catch(Exception e){
-			System.out.println("Dao Layer Exception : "+e);
+		Transaction transaction = null;
+		Session session = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			String hql = "FROM RawProjectInfo R WHERE R.coreUser = " + userid;
+			Query query = session.createQuery(hql);
+			List<RawProjectInfo> rawProjectInfoList = query.list();
+			transaction.commit();
+			return rawProjectInfoList;
+		} catch (Exception e) {
+			e.printStackTrace();
+			transaction.rollback();
 			return null;
 		}
-		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
