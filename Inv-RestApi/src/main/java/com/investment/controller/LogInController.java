38c4084ec054d1fb9ab2e55fe7eb8946d91e1a94
masith <masithprasanga@gmail.com>
2016-12-12 15:14:10 +0530

    LogInController Validations

--- Inv-RestApi/src/main/java/com/investment/controller/LogInController.java
+++ Inv-RestApi/src/main/java/com/investment/controller/LogInController.java
@@ -15,36 +15,60 @@ import com.investment.service.CoreUserService;
 import com.investment.service.UserRoleService;
 
 @RestController
-@RequestMapping("api/v1/user") 
+@RequestMapping("api/v1/user")
 public class LogInController {
-	
+
 	@Autowired
 	private CoreUserService coreUserService = null;
-	
-	@Autowired 
+
+	@Autowired
 	private UserRoleService userRoleService = null;
-	
+
 	@Autowired
 	private LogInHandler logInHandler = null;
-	
+
 	// SignUp EndPoint
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public ResponseEntity<Void> uploadUrls(@RequestBody CoreUserDto coreUserDto) {
-		
-		try{
+		try {
+			
+			if (coreUserDto.getUserEmail() == null || coreUserDto.getUserEmail().equals("") || coreUserDto.getPassword() == null || coreUserDto.getPassword().equals("")) {
+				System.out.println("Inside the Validation");
+				return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+			} 
+			
 			CoreUser user = coreUserService.findByEmail(coreUserDto.getUserEmail());
-			if(user != null){
+			if (user != null) {
+				System.out.println("Inside already reported");
 				return new ResponseEntity<Void>(HttpStatus.ALREADY_REPORTED);
 			}
+			
 			boolean status = logInHandler.createNewUser(coreUserDto);
+			
 			return new ResponseEntity<Void>(HttpStatus.CREATED);
-		}catch(Exception e){
+			
+		} catch (Exception e) {
+			System.out.println("Exception is : "+e);
+			e.printStackTrace();
 			return new ResponseEntity<Void>(HttpStatus.EXPECTATION_FAILED);
 		}
-		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
