3b99a417b28e5d3b6e2b0ec0d0d8df9726237097
masith <masithprasanga@gmail.com>
2016-12-12 18:08:20 +0530

    Added CoreUserResponseDto

---
+++ Inv-RestApi/src/main/java/com/investment/dto/response/CoreUserResponseDto.java
@@ -0,0 +1,53 @@
+package com.investment.dto.response;
+
+import java.io.Serializable;
+
+public class CoreUserResponseDto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
