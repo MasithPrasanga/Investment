90e0646054427cccb66e4bc114ccc7627b7b8dba
masith <masithprasanga@gmail.com>
2016-12-14 12:44:22 +0530

    Developed Reset Password End Point

--- Inv-RestApi/src/main/java/com/investment/controller/LogInController.java
+++ Inv-RestApi/src/main/java/com/investment/controller/LogInController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.investment.dto.CoreUserDto;
+import com.investment.dto.PasswordResetRequestDto;
 import com.investment.dto.response.CoreUserResponseDto;
+import com.investment.dto.response.PasswordResetResponseDto;
 import com.investment.dto.response.SignInResponseDto;
 import com.investment.entity.CoreUser;
 import com.investment.handler.LogInHandler;
@@ -42,7 +44,6 @@ public class LogInController {
 			if (user != null) {
 				return new ResponseEntity<Void>(HttpStatus.ALREADY_REPORTED);
 			}
-
 			boolean status = logInHandler.createNewUser(coreUserDto);
 			return new ResponseEntity<Void>(HttpStatus.CREATED);
 
@@ -60,34 +61,70 @@ public class LogInController {
 			if (coreUserDto.getUserEmail() == null || coreUserDto.getUserEmail().equals("")
 					|| coreUserDto.getPassword() == null || coreUserDto.getPassword().equals("")) {
 				signInResponse.setStatus(HttpStatus.BAD_REQUEST);
-				return new ResponseEntity<SignInResponseDto>(signInResponse,HttpStatus.BAD_REQUEST);
+				return new ResponseEntity<SignInResponseDto>(signInResponse, HttpStatus.BAD_REQUEST);
 			}
 			CoreUser user = coreUserService.findByEmail(coreUserDto.getUserEmail());
 			if (user == null) {
 				signInResponse.setStatus(HttpStatus.NOT_FOUND);
-				return new ResponseEntity<SignInResponseDto>(signInResponse,HttpStatus.NOT_FOUND);
+				return new ResponseEntity<SignInResponseDto>(signInResponse, HttpStatus.NOT_FOUND);
 			}
-			
-			if(user.getActivationStatus().equals(ApiConstants.ADMIN_NOT_APPROVED)){
+
+			if (user.getActivationStatus().equals(ApiConstants.ADMIN_NOT_APPROVED)) {
 				signInResponse.setStatus(HttpStatus.UNAUTHORIZED);
-				return new ResponseEntity<SignInResponseDto>(signInResponse,HttpStatus.UNAUTHORIZED);
+				return new ResponseEntity<SignInResponseDto>(signInResponse, HttpStatus.UNAUTHORIZED);
 			}
-			
-			
-			if(coreUserDto.getUserEmail().equals(user.getUserEmail())){
+
+			if (coreUserDto.getUserEmail().equals(user.getUserEmail())) {
 				StandardPasswordEncoder encoder = new StandardPasswordEncoder();
-				boolean correctPsd = encoder.matches(coreUserDto.getPassword(),user.getPassword());
-				if(correctPsd){
+				boolean correctPsd = encoder.matches(coreUserDto.getPassword(), user.getPassword());
+				if (correctPsd) {
 					signInResponse.setUserEmail(user.getUserEmail());
 					signInResponse.setStatus(HttpStatus.ACCEPTED);
 					signInResponse.setAccountType(user.getAccountType());
 				}
 			}
-			return new ResponseEntity<SignInResponseDto>(signInResponse,HttpStatus.ACCEPTED);
+			return new ResponseEntity<SignInResponseDto>(signInResponse, HttpStatus.ACCEPTED);
 
 		} catch (Exception e) {
 			signInResponse.setStatus(HttpStatus.EXPECTATION_FAILED);
-			return new ResponseEntity<SignInResponseDto>(signInResponse,HttpStatus.EXPECTATION_FAILED);
+			return new ResponseEntity<SignInResponseDto>(signInResponse, HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+	// Reset Password EndPoint
+	@RequestMapping(value = "/resetpassword", method = RequestMethod.POST)
+	public ResponseEntity<PasswordResetResponseDto> ResetPassword( @RequestBody PasswordResetRequestDto resetPasswordRequest) {
+		PasswordResetResponseDto passwordResetResponse = new PasswordResetResponseDto();
+		try {
+			if (resetPasswordRequest.getEmail() == null 
+					|| resetPasswordRequest.getEmail().equals("")
+					|| resetPasswordRequest.getCurrentPassword() == null
+					|| resetPasswordRequest.getCurrentPassword().equals("")
+					|| resetPasswordRequest.getNewPassword() == null
+					|| resetPasswordRequest.getNewPassword().equals("")) {
+
+				passwordResetResponse.setStatus(HttpStatus.BAD_REQUEST);
+				return new ResponseEntity<PasswordResetResponseDto>(passwordResetResponse, HttpStatus.BAD_REQUEST);
+			}
+			CoreUser user = coreUserService.findByEmail(resetPasswordRequest.getEmail());
+			if (user == null) {
+				passwordResetResponse.setStatus(HttpStatus.NOT_FOUND);
+				return new ResponseEntity<PasswordResetResponseDto>(passwordResetResponse, HttpStatus.NOT_FOUND);
+			}
+			StandardPasswordEncoder encoder = new StandardPasswordEncoder();
+			boolean correctPsd = encoder.matches(resetPasswordRequest.getCurrentPassword(), user.getPassword());
+			if (correctPsd) {	
+				StandardPasswordEncoder psdEncorder = new StandardPasswordEncoder();
+				String en = psdEncorder.encode(resetPasswordRequest.getNewPassword());
+				user.setPassword(en);	
+				coreUserService.update(user);
+				passwordResetResponse.setStatus(HttpStatus.OK);
+			}
+			return new ResponseEntity<PasswordResetResponseDto>(passwordResetResponse, HttpStatus.OK);
+
+		} catch (Exception e) {
+			passwordResetResponse.setStatus(HttpStatus.EXPECTATION_FAILED);
+			return new ResponseEntity<PasswordResetResponseDto>(passwordResetResponse, HttpStatus.EXPECTATION_FAILED);
 		}
 	}
 
