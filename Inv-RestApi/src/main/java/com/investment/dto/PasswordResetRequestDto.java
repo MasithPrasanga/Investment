3b4a492cb86abafe4448d8238b5999c4c301c5cc
masith <masithprasanga@gmail.com>
2016-12-14 12:38:56 +0530

    Added Password Reset Request Dto Class

---
+++ Inv-RestApi/src/main/java/com/investment/dto/PasswordResetRequestDto.java
@@ -0,0 +1,44 @@
+package com.investment.dto;
+
+import java.io.Serializable;
+
+public class PasswordResetRequestDto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	
+	private String currentPassword;
+	
+	private String newPassword;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCurrentPassword() {
+		return currentPassword;
+	}
+
+	public void setCurrentPassword(String currentPassword) {
+		this.currentPassword = currentPassword;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	
+}
+
+
+
+
+
