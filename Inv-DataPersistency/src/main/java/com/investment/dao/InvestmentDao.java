7bfd7889d31d70ea0bd7733fa98e0213df5079ba
masith <masithprasanga@gmail.com>
2016-12-15 16:18:49 +0530

    Updated Data Persistency Layer

--- Inv-DataPersistency/src/main/java/com/investment/dao/InvestmentDao.java
+++ Inv-DataPersistency/src/main/java/com/investment/dao/InvestmentDao.java
@@ -1,8 +1,11 @@
 package com.investment.dao;
 
+import java.util.List;
+
 import com.investment.dao.root.RootDao;
 import com.investment.entity.Investment;
 
 public interface InvestmentDao  extends RootDao<Investment>{
 
+	public List<Investment> findByInvestorId(Integer investorId);
 }
--- Inv-DataPersistency/src/main/java/com/investment/dao/InvestmentDaoImpl.java
+++ Inv-DataPersistency/src/main/java/com/investment/dao/InvestmentDaoImpl.java
@@ -1,5 +1,12 @@
 package com.investment.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.investment.dao.root.RootDaoImpl;
@@ -8,6 +15,29 @@ import com.investment.entity.Investment;
 @Repository
 public class InvestmentDaoImpl extends RootDaoImpl<Investment> implements InvestmentDao{
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	public List<Investment> findByInvestorId(Integer investorId) {
+		Transaction transaction = null;
+		Session session = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			String hql = "FROM Investment I WHERE I.investor = " + investorId;
+			Query query = session.createQuery(hql);
+			List<Investment> investedProjectList = query.list();
+			transaction.commit();
+			return investedProjectList;
+		} catch (Exception e) {
+			e.printStackTrace();
+			transaction.rollback();
+			return null;
+		}
+	}
 }
+
+
+
+
 
 
