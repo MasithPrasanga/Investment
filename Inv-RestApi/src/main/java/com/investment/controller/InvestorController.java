9525c7d8dd06fd527042ff9c35f395658e6b1cac
masith <masithprasanga@gmail.com>
2016-12-15 16:20:12 +0530

    Developed InvestedProject EndPoint

--- Inv-RestApi/src/main/java/com/investment/controller/InvestorController.java
+++ Inv-RestApi/src/main/java/com/investment/controller/InvestorController.java
@@ -6,14 +6,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.investment.dto.request.InvestRequestDto;
+import com.investment.dto.response.CoreUserResponseDto;
+import com.investment.dto.response.InvestedProjectsResponseDto;
 import com.investment.dto.response.InvestmentResponseDto;
 import com.investment.dto.response.ProssedProjectInfoResponseDto;
+import com.investment.entity.CoreUser;
+import com.investment.entity.Investment;
 import com.investment.entity.ProcessedProjectInfo;
 import com.investment.handler.InvestorHandler;
 import com.investment.service.InvestmentService;
@@ -83,12 +88,55 @@ public class InvestorController {
 		}
 	}
 
-	// Show All the Funded Project List (To be Implemented)
-	@RequestMapping(value = "/getfundedprojects", method = RequestMethod.GET)
-	public ResponseEntity<List<ProcessedProjectInfo>> getAllFundedProjects() {
-		return new ResponseEntity<List<ProcessedProjectInfo>>(HttpStatus.NO_CONTENT);
+	// Show All the invested Project List 
+	@RequestMapping(value = "/investedprojects/{id}", method = RequestMethod.POST)
+	public ResponseEntity<List<InvestedProjectsResponseDto>> getInvestedProjects(@PathVariable("id") int investorId) {
+		try{
+			List<Investment> investmentList = investmentService.findByInvestorId(investorId);
+			List<InvestedProjectsResponseDto> investedProjectList = new ArrayList<InvestedProjectsResponseDto>();
+			for(Investment inv : investmentList){
+				InvestedProjectsResponseDto investedProject = new InvestedProjectsResponseDto();
+				investedProject.setProjectName(inv.getProcessedProject().getProjectName());
+				investedProject.setSingleSharePrice(inv.getProcessedProject().getSharePrice());
+				investedProject.setImageUrl(inv.getProcessedProject().getImageUrl());
+				investedProject.setVideoUrl(inv.getProcessedProject().getVideoUrl());
+				investedProject.setFullAmmountCanInvest(inv.getProcessedProject().getFullAmmount());
+				investedProject.setNoOfSharesCanBuy(inv.getProcessedProject().getNoOfShares());
+				investedProject.setMininumAmmountCanInvest(inv.getProcessedProject().getMininumAmmount());
+				investedProject.setType(inv.getProcessedProject().getType().getType());
+				investedProject.setCurrency(inv.getProcessedProject().getCurrency().getName());
+				investedProject.setCustomerType(inv.getProcessedProject().getCustomerType().getType());
+				investedProject.setCategory(inv.getProcessedProject().getCategory().getName());
+				investedProject.setInvestedDate(inv.getInvestedDate());
+				investedProject.setInvestedAmouont(inv.getInvestedAmouont());
+				investedProject.setInvestedNoOfShares(inv.getNoOfShares());
+				investedProject.setInvestedPrecentage(inv.getPresentageOfFullAmount());
+				
+				CoreUserResponseDto entrepreneur = new CoreUserResponseDto();
+				CoreUser user = inv.getProcessedProject().getRawProjectInfo().getCoreUser();
+				entrepreneur.setFirstName(user.getFirstName());
+				entrepreneur.setLastName(user.getLastName());
+				entrepreneur.setUserEmail(user.getUserEmail());
+				entrepreneur.setMobileNumber(user.getMobileNumber());
+				entrepreneur.setLandNumber(user.getLandNumber());
+				entrepreneur.setBirthDate(user.getBirthDate());
+				entrepreneur.setGender(user.getGender());
+				entrepreneur.setAccountType(user.getAccountType());
+				investedProject.setEntrepreneur(entrepreneur);
+				
+				investedProjectList.add(investedProject);
+			}
+			
+			return new ResponseEntity<List<InvestedProjectsResponseDto>>(investedProjectList,HttpStatus.OK);
+		}catch(Exception e){
+			return new ResponseEntity<List<InvestedProjectsResponseDto>>(HttpStatus.EXPECTATION_FAILED);
+		}
 	}
 }
+
+
+
+
 
 
 
