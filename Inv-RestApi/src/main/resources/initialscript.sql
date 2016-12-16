b441d98e266b7b933b3cad36f15543c1c0e5b3d8
masith <masithprasanga@gmail.com>
2016-12-16 23:54:30 +0530

    Added Initial SQL Script

---
+++ Inv-RestApi/src/main/resources/initialscript.sql
@@ -0,0 +1,21 @@
+INSERT INTO category (id, name) VALUES (1, 'C 1');
+INSERT INTO category (id, name) VALUES (2, 'C 2');
+INSERT INTO category (id, name) VALUES (3, 'C 3');
+INSERT INTO category (id, name) VALUES (4, 'C 4');
+
+INSERT INTO currency (id, code, name) VALUES (1, 'LKR', 'Sri Lankan Rupee');
+INSERT INTO currency (id, code, name) VALUES (2, 'GBP', 'England Pound');
+
+INSERT INTO customer_type (id, type) VALUES (1, 'B2B');
+INSERT INTO customer_type (id, type) VALUES (2, 'B2C');
+INSERT INTO customer_type (id, type) VALUES (3, 'A2A');
+INSERT INTO customer_type (id, type) VALUES (4, 'A2B');
+
+
+INSERT INTO type (id, type) VALUES (1, 'Type 1');
+INSERT INTO type (id, type) VALUES (2, 'Type 2');
+INSERT INTO type (id, type) VALUES (3, 'Type 3');
+INSERT INTO type (id, type) VALUES (4, 'Type 4');
+INSERT INTO type (id, type) VALUES (5, 'Type 5');
+
+
