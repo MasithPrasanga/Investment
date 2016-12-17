6b69d2f36a431146789a8b96ee8e9b1288002c56
masith <masithprasanga@gmail.com>
2016-12-17 11:43:13 +0530

    Updated RootHandler

--- Inv-RestApi/src/main/java/com/investment/handler/root/RootHandler.java
+++ Inv-RestApi/src/main/java/com/investment/handler/root/RootHandler.java
@@ -1,7 +1,9 @@
 package com.investment.handler.root;
 
 import com.investment.dto.response.CoreUserResponseDto;
+import com.investment.dto.response.ProssedProjectInfoResponseDto;
 import com.investment.entity.CoreUser;
+import com.investment.entity.ProcessedProjectInfo;
 
 public class RootHandler {
 
@@ -15,6 +17,23 @@ public class RootHandler {
 		dto.setGender(user.getGender());
 		dto.setBirthDate(user.getBirthDate());
 		dto.setAccountType(user.getAccountType());
+		return dto;
+	}
+	
+	public ProssedProjectInfoResponseDto createProjectInfoResponse(ProcessedProjectInfo processedData){
+		ProssedProjectInfoResponseDto dto = new ProssedProjectInfoResponseDto();
+		dto.setId(processedData.getId());
+		dto.setProjectName(processedData.getProjectName());
+		dto.setSharePrice(processedData.getSharePrice());
+		dto.setImageUrl(processedData.getImageUrl());
+		dto.setVideoUrl(processedData.getVideoUrl());
+		dto.setFullAmmount(processedData.getFullAmmount());
+		dto.setNoOfShares(processedData.getNoOfShares());
+		dto.setMininumAmmount(processedData.getMininumAmmount());
+		dto.setCurrency(processedData.getCurrency());
+		dto.setCustomerType(processedData.getCustomerType());
+		dto.setType(processedData.getType());
+		dto.setCategory(processedData.getCategory());
 		return dto;
 	}
 }
